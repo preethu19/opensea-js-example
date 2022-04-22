c28bd3dc7cbd4fc7193d010e49de29417a39fee1
preethu19 <preethamkumar2000@gmail.com>
2022-04-22 11:31:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
+import * as Web3 from 'web3'
+import { OpenSeaPort, Network } from 'opensea-js'
 import './App.css';
 
+const provider = new Web3.providers.HttpProvider('https://mainnet.infura.io')
+
+const seaport = new OpenSeaPort(provider, {
+  networkName: Network.Main
+})
+
+
+
 function App() {
+
+  const [asset, setAsset] = useState();
+
+  useEffect( async()=>{
+    const a = await seaport.api.getAsset({
+      tokenAddress: "0x06012c8cf97bead5deae237070f9587f8e7a266d", // CryptoKitties
+      tokenId: "1", // Token ID
+    })
+    setAsset(a)
+    console.log(a)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 }
